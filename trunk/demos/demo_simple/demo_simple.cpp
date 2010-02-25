11a3e5d357dd4ce6bf21d01bfb0c48f4f89e014c
kspes <kspes@669e1989-cafa-1570-9272-bcdd0d0ecbcf>
2010-02-25 14:25:47 +0000

    * added exception handling code to demo_simple.cpp * vert formatting: center added * exposed exception classes

--- trunk/demos/demo_simple/demo_simple.cpp
+++ trunk/demos/demo_simple/demo_simple.cpp
@@ -23,6 +23,7 @@ http://www.gnu.org/copyleft/lesser.txt.
 #include "aprilui/Dataset.h"
 #include "aprilui/Objects.h"
 #include "atres/Atres.h"
+#include <iostream>
 
 AprilUI::Dataset* dataset;
 
@@ -39,17 +40,23 @@ bool render(float time_increase)
 
 int main()
 {
-	April::init("OpenGL",800,600,0,"demo_simple");
-	rendersys->registerUpdateCallback(render);
-	AprilUI::init();
-	dataset=new AprilUI::Dataset("../media/demo_simple.datadef");
-	dataset->load();
-	Atres::loadFont("../media/arial.font");
+	try
+	{
+		April::init("OpenGL",800,600,0,"demo_simple");
+		rendersys->registerUpdateCallback(render);
+		AprilUI::init();
+		dataset=new AprilUI::Dataset("../media/demo_simple.datadef");
+		dataset->load();
+		Atres::loadFont("../media/arial.font");
 
-	rendersys->enterMainLoop();
-	delete dataset;
-	AprilUI::destroy();
-	April::destroy();
-
+		rendersys->enterMainLoop();
+		delete dataset;
+		AprilUI::destroy();
+		April::destroy();
+	}
+	catch (AprilUI::_GenericException e)
+	{
+		std::cout << e.repr() << "\n";
+	}
 	return 0;
 }
