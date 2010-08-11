45946162f7cf45b8e539af15fe9b22626e165c05
borisblizzard <borisblizzard@669e1989-cafa-1570-9272-bcdd0d0ecbcf>
2010-08-11 12:29:59 +0000

    - implemented an advanced version of the EditBox

--- trunk/include/aprilui/ObjectEditBox.h
+++ trunk/include/aprilui/ObjectEditBox.h
@@ -21,16 +21,40 @@ namespace AprilUI
 	{
 	protected:
 		bool mPushed;
-		//int mCaratIndex;
+		int mCursorIndex;
+		int mMaxLength;
+		char mPasswordChar;
+		bool mCtrlMode;
+		hstr mFilter;
+		
 		void OnDraw(float offset_x,float offset_y);
+		
+		void _cursorMoveLeftWord();
+		void _cursorMoveRightWord();
+		void _deleteLeft(int count=1);
+		void _deleteRight(int count=1);
+		void _deleteLeftWord();
+		void _deleteRightWord();
+		void _insertText(chstr text);
 	public:
 		EditBox(chstr name,float x,float y,float w,float h);
 		
 		bool OnMouseDown(int button,float x,float y);
 		bool OnMouseUp(int button,float x,float y);
 		void OnKeyDown(unsigned int keycode);
+		void OnKeyUp(unsigned int keycode);
 		void OnChar(unsigned int charcode);
 		void setProperty(chstr name,chstr value);
+		
+		int getCursorIndex() { return mCursorIndex; }
+		void setCursorIndex(int cursorIndex);
+		int getMaxLength() { return mMaxLength; }
+		void setMaxLength(int maxLength);
+		char getPasswordChar() { return mPasswordChar; }
+		void setPasswordChar(char passwordChar) { mPasswordChar=passwordChar; }
+		hstr getFilter() { return mFilter; }
+		void setFilter(chstr filter);
+		void setText(chstr text);
 		
 	};
 
--- trunk/src/objects/ObjectEditBox.cpp
+++ trunk/src/objects/ObjectEditBox.cpp
@@ -10,6 +10,7 @@ Copyright (c) 2010 Kresimir Spes (kreso@cateia.com), Boris Mikic                
 #include <april/Keys.h>
 #include <atres/Atres.h>
 #include <hltypes/hstring.h>
+#include <hltypes/util.h>
 
 #include "Dataset.h"
 #include "ObjectEditBox.h"
@@ -25,19 +26,67 @@ namespace AprilUI
 		mText="EditBox: "+name;
 		mTypeName="EditBox";
 		mPushed=0;
-		//mCaratIndex=0;
+		mMaxLength=0;
+		mPasswordChar=0;
+		mCursorIndex=0;
+		mCtrlMode=0;
+		mFilter="";
 	}
 
 	void EditBox::OnDraw(float offset_x,float offset_y)
 	{
-		unsigned char a=mTextColor.a;
 		rendersys->drawColoredQuad(mX+offset_x, mY+offset_y, mWidth, mHeight, 0, 0, 0, 0.7f+0.3f*mPushed);
+		hstr text=mText;
+		if (mPasswordChar && mText != "")
+		{
+			mText=hstr(mPasswordChar,mText.size());
+		}
 		Label::OnDraw(offset_x,offset_y);
+		//2DO - add cursor drawing
+		//rendersys->drawColoredQuad(x, y, 1, 0.8*font_h, 1, 1, 1, 1);
+		mText=text;
 	}
 	
 	void EditBox::setProperty(chstr name,chstr value)
 	{
 		Label::setProperty(name,value);
+		if      (name == "max_length")    setMaxLength(value);
+		else if (name == "password_char") setPasswordChar(value.c_str()[0]);
+		else if (name == "filter")        setFilter(value);
+	}
+	
+	void EditBox::setCursorIndex(int cursorIndex)
+	{
+		mCursorIndex=hclamp(cursorIndex,0,mText.size());
+	}
+	
+	void EditBox::setMaxLength(int maxLength)
+	{
+		mMaxLength=maxLength;
+		if (mMaxLength > 0 && mText.size() > mMaxLength)
+		{
+			mText=mText(0,mMaxLength);
+			setCursorIndex(mCursorIndex);
+		}
+	}
+	
+	void EditBox::setFilter(chstr filter)
+	{
+		mFilter=filter;
+		setText(mText);
+	}
+	
+	void EditBox::setText(chstr text)
+	{
+		Label::setText(text);
+		harray<char> chars=harray<char>(mText.c_str(),mText.size());
+		foreach (char,it,chars)
+		{
+			if (!mFilter.contains(*it))
+			{
+				mText.replace(*it,"");
+			}
+		}
 	}
 
 	bool EditBox::OnMouseDown(int button,float x,float y)
@@ -69,22 +118,123 @@ namespace AprilUI
 	{
 		switch (keycode)
 		{
+		case AK_LEFT:
+			mCtrlMode ? _cursorMoveLeftWord() : setCursorIndex(mCursorIndex-1);
+			break;
+		case AK_RIGHT:
+			mCtrlMode ? _cursorMoveRightWord() : setCursorIndex(mCursorIndex+1);
+			break;
 		case AK_BACK:
-			if (this->mText.size() > 0)
-			{
-				this->mText = this->mText(0, this->mText.size() - 1);
-			}
+			mCtrlMode ? _deleteLeftWord() : _deleteLeft();
+			break;
+		case AK_DELETE:
+			mCtrlMode ? _deleteRightWord() : _deleteRight();
+			break;
+		case AK_HOME:
+			setCursorIndex(0);
+			break;
+		case AK_END:
+			setCursorIndex(mText.size());
+			break;
+		case AK_CONTROL:
+			mCtrlMode=1;
+			break;
+		}
+	}
+
+	void EditBox::OnKeyUp(unsigned int keycode)
+	{
+		switch (keycode)
+		{
+		case AK_CONTROL:
+			mCtrlMode=0;
 			break;
 		}
 	}
 
 	void EditBox::OnChar(unsigned int charcode)
 	{
-		//2DO - change to: if charcode exists in Font Manager
-		if (isprint(charcode))
+		char c=(char)charcode;
+		//2DO - add: if charcode exists in Font Manager
+		if (mFilter == "" || mFilter.contains(c))
 		{
-			this->mText += (char)charcode;
+			_insertText(c);
 		}
 	}
+	
+	void EditBox::_cursorMoveLeftWord()
+	{
+		while (mCursorIndex > 0 && mText[mCursorIndex-1] == ' ') mCursorIndex--;
+		while (mCursorIndex > 0 && mText[mCursorIndex-1] != ' ') mCursorIndex--;
+	}
+	
+	void EditBox::_cursorMoveRightWord()
+	{
+		while (mCursorIndex <= mText.size() && mText[mCursorIndex] != ' ') mCursorIndex++;
+		while (mCursorIndex <= mText.size() && mText[mCursorIndex] == ' ') mCursorIndex++;
+	}
+	
+	void EditBox::_deleteLeft(int count)
+	{
+		if (mCursorIndex==0)
+		{
+			return;
+		}
+		count=hmin(count,mCursorIndex);
+		hstr left=(mCursorIndex > count ? mText(0,mCursorIndex-count) : "");
+		hstr right=(mCursorIndex < mText.size() ? mText(mCursorIndex,mText.size()-mCursorIndex) : "");
+		mCursorIndex-=count;
+		mText=left+right;
+	}
+	
+	void EditBox::_deleteRight(int count)
+	{
+		count=hmin(count,mText.size()-mCursorIndex);
+		mCursorIndex+=count;
+		_deleteLeft(count);
+	}
+	
+	void EditBox::_deleteLeftWord()
+	{
+		int index=mCursorIndex;
+		while (index > 0 && mText[index-1] == ' ') index--;
+		while (index > 0 && mText[index-1] != ' ') index--;
+		if (mCursorIndex > index)
+		{
+			_deleteLeft(mCursorIndex-index);
+		}
+	}
+	
+	void EditBox::_deleteRightWord()
+	{
+		int index=mCursorIndex;
+		while (index <= mText.size() && mText[index] != ' ') index++;
+		while (index <= mText.size() && mText[index] == ' ') index++;
+		if (index > mCursorIndex)
+		{
+			_deleteLeft(index-mCursorIndex);
+		}
+	}
+	
+	void EditBox::_insertText(chstr text)
+	{
+		hstr new_text=text;
+		if (mMaxLength > 0)
+		{
+			if (mText.size() >= mMaxLength)
+			{
+				return;
+			}
+			if (mText.size() + new_text.size() > mMaxLength)
+			{
+				new_text=new_text(0,mMaxLength-mText.size());
+			}
+		}
+		hstr left=(mCursorIndex > 0 ? mText(0,mCursorIndex) : "");
+		hstr right=(mCursorIndex < mText.size() ? mText(mCursorIndex,mText.size()-mCursorIndex) : "");
+		mCursorIndex+=new_text.size();
+		mText=left+new_text+right;
+	}
+	
 
 }
