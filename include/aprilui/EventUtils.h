a06d2bae762c003da97758caad209fe4f8e374ab
kspes <kresimir.spes@gmail.com>
2011-05-24 09:05:45 +0000

    _SET_CLICK_EVENT and _SET_MOUSEDOWN_EVENT macros in EventUtils.h changed

--- include/aprilui/EventUtils.h
+++ include/aprilui/EventUtils.h
@@ -19,10 +19,10 @@ Copyright (c) 2010 Kresimir Spes, Boris Mikic                                   
 #define SET_MOUSEDOWN_EVENT(dataset,obj_name,cls,member_func) \
         dataset->getObject(obj_name)->registerEvent("MouseDown",new aprilui::MemberCallbackEvent(&cls::member_func,(cls*) this));
 // for class member functions being registered from within the class
-#define _SET_CLICK_EVENT(obj,member_func) \
-		obj->registerEvent("Click",new aprilui::MemberCallbackEvent(member_func,this));
-#define _SET_MOUSEDOWN_EVENT(obj,member_func) \
-        obj->registerEvent("MouseDown",new aprilui::MemberCallbackEvent(member_func,this));
+#define _SET_CLICK_EVENT(obj,cls,member_func) \
+		obj->registerEvent("Click",new aprilui::MemberCallbackEvent(&cls::member_func,(cls*) this));
+#define _SET_MOUSEDOWN_EVENT(obj,cls,member_func) \
+        obj->registerEvent("MouseDown",new aprilui::MemberCallbackEvent(&cls::member_func,(cls*) this));
 // for free C functions and static class functions
 #define SET_CLICK_EVENT_FUNCTION(dataset,obj_name,free_function) \
 		dataset->getObject(obj_name)->registerEvent("Click",new aprilui::CallbackEvent(free_function));
