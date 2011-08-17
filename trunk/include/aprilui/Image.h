0ce190af9244770d586bde792764b116a19321c5
borisblizzard <borisblizzard@669e1989-cafa-1570-9272-bcdd0d0ecbcf>
2011-08-17 13:11:33 +0000

    - small update

--- trunk/include/aprilui/Image.h
+++ trunk/include/aprilui/Image.h
@@ -26,7 +26,7 @@ namespace aprilui
 	{
 	public:
 		Image(april::Texture* texture, chstr name, grect source, bool vertical = false, bool invertX = false, bool invertY = false);
-		Image(Image& img,chstr name);
+		Image(Image& img, chstr name);
 		virtual ~Image();
 		
 		virtual void draw(grect rect, april::Color color = APRIL_COLOR_WHITE);
