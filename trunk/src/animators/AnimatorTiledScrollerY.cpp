26579ab5c0a0f7fadddea075a0d1e91bd3ae89fc
borisblizzard <borisblizzard@669e1989-cafa-1570-9272-bcdd0d0ecbcf>
2011-11-15 10:02:28 +0000

    - implemented TiledScrollerX - implemented TiledScrollerY

---
+++ trunk/src/animators/AnimatorTiledScrollerY.cpp
@@ -0,0 +1,66 @@
+/************************************************************************************\
+This source file is part of the APRIL User Interface Library                         *
+For latest info, see http://libaprilui.sourceforge.net/                              *
+**************************************************************************************
+Copyright (c) 2011 Boris Mikic                                        *
+*                                                                                    *
+* This program is free software; you can redistribute it and/or modify it under      *
+* the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php   *
+\************************************************************************************/
+#include <gtypes/Rectangle.h>
+#include <hltypes/hstring.h>
+#include <hltypes/util.h>
+
+#include "aprilui.h"
+#include "AnimatorTiledScrollerY.h"
+#include "ObjectImageBox.h"
+#include "TiledImage.h"
+
+namespace aprilui
+{
+	namespace Animators
+	{
+		TiledScrollerY::TiledScrollerY(chstr name) : Animator("TiledScrollerY", name)
+		{
+		}
+
+		void TiledScrollerY::notifyEvent(chstr name, void* params)
+		{
+			if (name == "AttachToObject" || name == "OnDelayEnd" && mInheritValue)
+			{
+				mValue = mOffset = mParent->getY();
+				if (mUseTarget)
+				{
+					mAmplitude = mTarget - mValue;
+				}
+			}
+			Object::notifyEvent(name, params);
+		}
+		
+		void TiledScrollerY::update(float k)
+		{
+			bool animated = this->isAnimated();
+			Animator::update(k);
+			if (!animated)
+			{
+				return;
+			}
+			ImageBox* imageBox = dynamic_cast<ImageBox*>(mParent);
+			if (imageBox == NULL)
+			{
+				aprilui::log("Animators::TiledScrollerY: parent object not a subclass of Objects::ImageBox!");
+				return;
+			}
+			TiledImage* image = dynamic_cast<TiledImage*>(imageBox->getImage());
+			if (image == NULL)
+			{
+				aprilui::log("Animators::TiledScrollerY: image in object not a subclass of Animators::TiledImage!");
+				return;
+			}
+			mValue = image->getScrollY();
+			mValue = _calculateValue(mTimeSinceLastFrame);
+			image->setScrollY(mValue);
+		}
+		
+	}
+}
