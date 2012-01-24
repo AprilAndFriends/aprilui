2d26dea99eb2a4283a39d10155872a0882df8d31
Boris Mikic <boris.blizzard@gmail.com>
2012-01-24 14:46:18 +0000

    - hltypes update (renamed util.h to hltypesUtil.h)

--- src/images/Image.cpp
+++ src/images/Image.cpp
@@ -12,7 +12,7 @@ Copyright (c) 2010 Kresimir Spes, Boris Mikic                                   
 #include <april/RenderSystem.h>
 #include <gtypes/Rectangle.h>
 #include <gtypes/Vector2.h>
-#include <hltypes/util.h>
+#include <hltypes/hltypesUtil.h>
 
 #include "Exception.h"
 #include "Image.h"
