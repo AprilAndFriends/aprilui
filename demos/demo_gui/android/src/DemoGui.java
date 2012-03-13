b2b88f99a9113bc68f53e9a2e06a5d4d8f60c0fe
Boris Mikic <boris.blizzard@gmail.com>
2012-03-13 11:17:53 +0000

    - april Android update

--- demos/demo_gui/android/src/DemoGui.java
+++ demos/demo_gui/android/src/DemoGui.java
@@ -7,4 +7,11 @@ public class DemoGui extends net.sourceforge.april.AprilActivity
 		System.loadLibrary("demo_gui");
 	}
 	
+	@Override
+	protected void onCreate(android.os.Bundle savedInstanceState)
+	{
+		this.forceArchivePath(this.getPackageResourcePath()); // forces APK as archive file
+		super.onCreate(savedInstanceState);
+	}
+	
 }
