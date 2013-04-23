b458ff3bef80ceea3376d71c9cadf5065ff5b15b
Boris Mikic <boris.blizzard@gmail.com>
2013-04-23 11:51:22 +0000

    - fixed small include mistake

--- src/QueuedCallbackEvent.cpp
+++ src/QueuedCallbackEvent.cpp
@@ -1,7 +1,9 @@
 #include <hltypes/exception.h>
-#include <aprilui/Object.h>
-#include <aprilui/EventArgs.h>
+#include <hltypes/hstring.h>
+
 #include "Dataset.h"
+#include "EventArgs.h"
+#include "Object.h"
 #include "QueuedCallbackEvent.h"
 
 namespace aprilui
@@ -23,13 +25,12 @@ namespace aprilui
 
 	void QueuedCallbackEvent::execute(void* params)
 	{
-		aprilui::EventArgs* args = (aprilui::EventArgs*) params;
+		aprilui::EventArgs* args = (aprilui::EventArgs*)params;
 		this->dataset = dynamic_cast<Dataset*>(args->object->getDataset());
 		if (this->dataset == NULL)
 		{
 			throw hl_exception("QueuedCallbackEvent: unable to process queued event, object's dataset is not a Chapter class.");
 		}
-		
 		this->dataset->queueCallback(this->event, new aprilui::EventArgs(args->object, args->x, args->y, args->keyCode, args->extra));
 	}
 
