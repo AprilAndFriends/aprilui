8b96044032135a96d0c737937241182ec7b4676a
borisblizzard <borisblizzard@669e1989-cafa-1570-9272-bcdd0d0ecbcf>
2013-08-02 10:50:50 +0000

    - demo update

--- trunk/demos/demo_tiledimage/demo_tiledimage.cpp
+++ trunk/demos/demo_tiledimage/demo_tiledimage.cpp
@@ -123,6 +123,7 @@ void april_init(const harray<hstr>& args)
 		april::createWindow((int)drawRect.w, (int)drawRect.h, false, "demo_tileimage");
 		atres::init();
 		aprilui::init();
+		aprilui::setViewport(viewport);
 		aprilui::setLocalization("en");
 		april::window->setUpdateDelegate(updateDelegate);
 		dataset = new aprilui::Dataset(RESOURCE_PATH "demo_tiledimage.dts");
