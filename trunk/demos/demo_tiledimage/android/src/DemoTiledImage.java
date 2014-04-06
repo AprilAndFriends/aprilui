e9c45e0043532aea8671539df5d659c46ef5464e
boris.blizzard <boris.blizzard@669e1989-cafa-1570-9272-bcdd0d0ecbcf>
2014-04-06 12:37:16 +0000

    - demos update

--- trunk/demos/demo_tiledimage/android/src/DemoTiledImage.java
+++ trunk/demos/demo_tiledimage/android/src/DemoTiledImage.java
@@ -1,6 +1,6 @@
 package com.example.aprilui.android.demoTiledImage;
 
-public class DemoTiledImage extends net.sourceforge.april.android.Activity
+public class DemoTiledImage extends com.googlecode.april.android.Activity
 {
 	static
 	{
@@ -11,7 +11,7 @@ public class DemoTiledImage extends net.sourceforge.april.android.Activity
 	protected void onCreate(android.os.Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		this.forceArchivePath(net.sourceforge.april.android.NativeInterface.ApkPath); // forces APK as archive file
+		this.forceArchivePath(com.googlecode.april.android.NativeInterface.ApkPath); // forces APK as archive file
 	}
 	
 }
