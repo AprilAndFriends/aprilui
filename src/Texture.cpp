c15c0301760d46f72abb9889662cb52b9d93b10a
kresimir.spes <kresimir.spes@gmail.com>
2014-05-14 13:23:20 +0000

    api fix

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -7,7 +7,6 @@
 /// This program is free software; you can redistribute it and/or modify it under
 /// the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
 
-#include <april/RamTexture.h>
 #include <april/RenderSystem.h>
 
 #include "aprilui.h"
