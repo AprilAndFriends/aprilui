004050b187d1c88c0d2505dda22e90fd5d035e44
boris.blizzard <boris.blizzard@669e1989-cafa-1570-9272-bcdd0d0ecbcf>
2014-12-29 14:20:35 +0000

    - hltypes exceptions update

--- trunk/include/aprilui/Exception.h
+++ trunk/include/aprilui/Exception.h
@@ -51,34 +51,34 @@ namespace aprilui
 	};
 
 	_EXCEPTION_CLASS_3(ObjectExistsException, type, name, filename);
-	#define ObjectExistsException(type, name, filename) _ObjectExistsException(type, name, filename, __FILE__, __LINE__)
+	#define ObjectExistsException(type, name, filename) aprilui::_ObjectExistsException(type, name, filename, __FILE__, __LINE__)
 
 	_EXCEPTION_CLASS_3(ObjectNotExistsException, type, name, filename);
-	#define ObjectNotExistsException(type, name, filename) _ObjectNotExistsException(type, name, filename, __FILE__, __LINE__)
+	#define ObjectNotExistsException(type, name, filename) aprilui::_ObjectNotExistsException(type, name, filename, __FILE__, __LINE__)
 
 	_EXCEPTION_CLASS_1(FontExistsException, fontname);
-	#define FontExistsException(fontname) _FontExistsException(fontname, __FILE__, __LINE__)
+	#define FontExistsException(fontname) aprilui::_FontExistsException(fontname, __FILE__, __LINE__)
 
 	_EXCEPTION_CLASS_1(FontNotExistsException, fontname);
-	#define FontNotExistsException(fontname) _FontNotExistsException(fontname, __FILE__, __LINE__)
+	#define FontNotExistsException(fontname) aprilui::_FontNotExistsException(fontname, __FILE__, __LINE__)
 
 	_EXCEPTION_CLASS_3(InvalidObjectTypeCast, type, objectName, datasetName);
-	#define InvalidObjectTypeCast(type, objectName, datasetName) _InvalidObjectTypeCast(type, objectName, datasetName, __FILE__, __LINE__)
+	#define InvalidObjectTypeCast(type, objectName, datasetName) aprilui::_InvalidObjectTypeCast(type, objectName, datasetName, __FILE__, __LINE__)
 
 	_EXCEPTION_CLASS_2(ObjectHasParentException, child, parent);
-	#define ObjectHasParentException(child, parent) _ObjectHasParentException(child, parent, __FILE__, __LINE__)
+	#define ObjectHasParentException(child, parent) aprilui::_ObjectHasParentException(child, parent, __FILE__, __LINE__)
 
 	_EXCEPTION_CLASS_1(ObjectWithoutParentException, child);
-	#define ObjectWithoutParentException(child) _ObjectWithoutParentException(child, __FILE__, __LINE__)
+	#define ObjectWithoutParentException(child) aprilui::_ObjectWithoutParentException(child, __FILE__, __LINE__)
 
 	_EXCEPTION_CLASS_2(ObjectNotChildException, child, parent);
-	#define ObjectNotChildException(child, parent) _ObjectNotChildException(child, parent, __FILE__, __LINE__)
+	#define ObjectNotChildException(child, parent) aprilui::_ObjectNotChildException(child, parent, __FILE__, __LINE__)
 
 	_EXCEPTION_CLASS_2(ObjectFactoryExistsException, type, name);
-	#define ObjectFactoryExistsException(type, name) _ObjectFactoryExistsException(type, name, __FILE__, __LINE__)
+	#define ObjectFactoryExistsException(type, name) aprilui::_ObjectFactoryExistsException(type, name, __FILE__, __LINE__)
 
 	_EXCEPTION_CLASS_2(ObjectFactoryNotExistsException, type, name);
-	#define ObjectFactoryNotExistsException(type, name) _ObjectFactoryNotExistsException(type, name, __FILE__, __LINE__)
+	#define ObjectFactoryNotExistsException(type, name) aprilui::_ObjectFactoryNotExistsException(type, name, __FILE__, __LINE__)
 
 }
 
