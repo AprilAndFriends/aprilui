a40752563b879c48cb4740f5a828a5093ec8d85c
Boris Mikic <boris.blizzard@gmail.com>
2015-01-18 19:36:19 +0000

    - small fix

--- src/Dataset.cpp
+++ src/Dataset.cpp
@@ -380,7 +380,7 @@ namespace aprilui
 					foreach_xmlproperty (prop, child)
 					{
 						name = prop->name();
-						if (name == "name" || name == "rect" || name == "position" || name == "size" || name == "x" || name == "y" || name == "w" || name == "h")
+						if (name == "name" || name == "rect" || name == "position" || name == "size" || name == "x" || name == "y" || name == "w" || name == "h" || name == "tile" || name == "tile_w" || name == "tile_h")
 						{
 							continue; // TODO - should be done better, maybe reading parameters from a list, then removing them so they aren't set more than once
 						}
