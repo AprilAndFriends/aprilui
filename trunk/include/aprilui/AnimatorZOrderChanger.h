d8c5f324a2b287f5c488a9e378c4fd95f68054b2
boris.blizzard <boris.blizzard@669e1989-cafa-1570-9272-bcdd0d0ecbcf>
2015-05-26 08:41:52 +0000

    - added ZOrderChanger animator

---
+++ trunk/include/aprilui/AnimatorZOrderChanger.h
@@ -0,0 +1,44 @@
+/// @file
+/// @version 4.05
+/// 
+/// @section LICENSE
+/// 
+/// This program is free software; you can redistribute it and/or modify it under
+/// the terms of the BSD license: http://opensource.org/licenses/BSD-3-Clause
+/// 
+/// @section DESCRIPTION
+/// 
+/// Defines an animator that can change the z-order of objects.
+
+#ifndef APRILUI_Z_ORDER_CHANGER_H
+#define APRILUI_Z_ORDER_CHANGER_H
+
+#include <hltypes/hstring.h>
+
+#include "Animator.h"
+
+namespace aprilui
+{
+	namespace Animators
+	{
+		class apriluiExport ZOrderChanger : public Animator
+		{
+			APRILUI_CLONEABLE(ZOrderChanger);
+		public:
+			ZOrderChanger(chstr name);
+			~ZOrderChanger();
+			inline hstr getClassName() const { return "ZOrderChanger"; }
+
+			static Animator* createInstance(chstr name);
+			
+		protected:
+			float _getObjectValue();
+			void _setObjectValue(float value);
+			
+			void _update(float timeDelta);
+
+		};
+	}
+}
+
+#endif
