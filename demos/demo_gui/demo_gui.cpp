1a5b832bc1029f58ca22532a336b75dfbf918296
Kresimir Spes <kresimir.spes@gmail.com>
2016-06-09 14:26:36 +0200

    added xcode target for gui demo

--- demos/demo_gui/demo_gui.cpp
+++ demos/demo_gui/demo_gui.cpp
@@ -6,22 +6,24 @@
 /// This program is free software; you can redistribute it and/or modify it under
 /// the terms of the BSD license: http://opensource.org/licenses/BSD-3-Clause
 
-#ifndef _ANDROID
-#ifndef _WINRT
-#define RESOURCE_PATH "../../demos/media/"
+#if defined(__APPLE__)
+	#define RESOURCE_PATH "media/"
+#elif !defined(_ANDROID)
+	#ifndef _WINRT
+		#define RESOURCE_PATH "../../demos/media/"
+	#else
+		#define RESOURCE_PATH "media/"
+	#endif
 #else
-#define RESOURCE_PATH "media/"
-#endif
-#elif defined(__APPLE__)
-#define RESOURCE_PATH "media/"
-#else
-#define RESOURCE_PATH "./"
+	#define RESOURCE_PATH "./"
 #endif
 
 #include <stdio.h>
 
 #ifdef __APPLE__
-#include <CoreFoundation/CoreFoundation.h>
+	#import <CoreFoundation/CoreFoundation.h>
+	#import <Foundation/Foundation.h>
+	#include <hltypes/hdir.h>
 #endif
 
 #define __APRIL_SINGLE_INSTANCE_NAME "demo_gui"
@@ -239,51 +241,30 @@ static UpdateDelegate* updateDelegate = NULL;
 static KeyboardDelegate* keyboardDelegate = NULL;
 static MouseDelegate* mouseDelegate = NULL;
 
-void april_init(const harray<hstr>& args)
+static void ObjCUtil_setCWD(const char* override_default_dir)
 {
-#ifdef __APPLE__
-	// On MacOSX, the current working directory is not set by
-	// the Finder, since you are expected to use Core Foundation
-	// or ObjC APIs to find files. 
-	// So, when porting you probably want to set the current working
-	// directory to something sane (e.g. .../Resources/ in the app
-	// bundle).
-	// In this case, we set it to parent of the .app bundle.
-	{	// curly braces in order to localize variables 
-
-		CFURLRef url = CFBundleCopyBundleURL(CFBundleGetMainBundle());
-		CFStringRef path = CFURLCopyFileSystemPath(url, kCFURLPOSIXPathStyle);
-		// let's hope chdir() will be happy with utf8 encoding
-		const char* cpath = CFStringGetCStringPtr(path, kCFStringEncodingUTF8);
-		char* cpath_alloc = NULL;
-		if (cpath == NULL)
+	static bool set = 0;
+	if (!set || override_default_dir != NULL)
+	{
+		if (override_default_dir == NULL)
 		{
-			// CFStringGetCStringPtr is allowed to return NULL. bummer.
-			// we need to use CFStringGetCString instead.
-			cpath_alloc = (char*)malloc(CFStringGetLength(path) + 1);
-			CFStringGetCString(path, cpath_alloc, CFStringGetLength(path) + 1, kCFStringEncodingUTF8);
+			NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+			const char* dir = [[[NSBundle mainBundle] resourcePath] UTF8String];
+			hdir::chdir(dir);
+			[pool release];
 		}
 		else
 		{
-			// even though it didn't return NULL, we still want to slice off bundle name.
-			cpath_alloc = (char*)malloc(CFStringGetLength(path) + 1);
-			strcpy(cpath_alloc, cpath);
+			hdir::chdir(override_default_dir);
 		}
-		// just in case / is appended to .app path for some reason
-		if (cpath_alloc[CFStringGetLength(path) - 1] == '/')
-		{
-			cpath_alloc[CFStringGetLength(path) - 1] = 0;
-		}
-		// replace pre-.app / with a null character, thus
-		// cutting off .app's name and getting parent of .app.
-		strrchr(cpath_alloc, '/')[0] = 0;
-		// change current dir using posix api
-		chdir(cpath_alloc);
-		free(cpath_alloc); // even if null, still ok
-		CFRelease(path);
-		CFRelease(url);
+		set = 1;
 	}
-#endif
+}
+
+void april_init(const harray<hstr>& args)
+{
+	ObjCUtil_setCWD(nil);
+	
 	updateDelegate = new UpdateDelegate();
 	keyboardDelegate = new KeyboardDelegate();
 	mouseDelegate = new MouseDelegate();
