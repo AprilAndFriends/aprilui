909557888bf679040e9ca9c5ba84bc0a392193fd
Boris Mikic <boris.blizzard@gmail.com>
2016-08-30 08:05:28 +0200

    - small update

--- demos/demo_simple/demo_simple.cpp
+++ demos/demo_simple/demo_simple.cpp
@@ -121,7 +121,7 @@ void april_init(const harray<hstr>& args)
 #if defined(_ANDROID) || defined(_IOS) || defined(_WINRT)
 		drawRect.setSize(april::getSystemInfo().displayResolution);
 #endif
-		april::init(april::RS_DEFAULT, april::WS_DEFAULT);
+		april::init(april::RenderSystemType::Default, april::WindowType::Default);
 		april::createRenderSystem();
 		april::createWindow((int)drawRect.w, (int)drawRect.h, false, "AprilUI: Simple Demo");
 		atres::init();
