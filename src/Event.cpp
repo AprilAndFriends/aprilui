968de58cea88fffb48634a98de78606486695ba2
Boris Mikic <boris.blizzard@gmail.com>
2016-12-19 13:48:53 +0100

    - code updates

--- src/Event.cpp
+++ src/Event.cpp
@@ -5,31 +5,17 @@
 /// 
 /// This program is free software; you can redistribute it and/or modify it under
 /// the terms of the BSD license: http://opensource.org/licenses/BSD-3-Clause
-/// 
-/// @section DESCRIPTION
-/// 
-/// Defines an event.
-
-#ifndef APRILUI_EVENT_H
-#define APRILUI_EVENT_H
 
 #include <hltypes/hstring.h>
 
-#include "apriluiExport.h"
+#include "Event.h"
 
 namespace aprilui
 {
-	class apriluiExport Event
+	hstr Event::Click = "Click";
+
+	Event::~Event()
 	{
-	public:
-		static hstr Click;
-
-		virtual ~Event();
-
-		virtual void execute(void* params) = 0;
-
-	};
+	}
 
 }
-
-#endif
