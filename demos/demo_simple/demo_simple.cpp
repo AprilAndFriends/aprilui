defb83031fd0880e244b565b3b44e1a143a8980e
Boris Mikic <boris.blizzard@gmail.com>
2018-06-19 08:28:56 +0200

    - changed preprocessor _ANDROID to __ANDROID__

--- demos/demo_simple/demo_simple.cpp
+++ demos/demo_simple/demo_simple.cpp
@@ -6,16 +6,16 @@
 /// This program is free software; you can redistribute it and/or modify it under
 /// the terms of the BSD license: http://opensource.org/licenses/BSD-3-Clause
 
-#ifndef _ANDROID
-#ifndef _WINRT
-#define RESOURCE_PATH "../../demos/media/"
-#else
-#define RESOURCE_PATH "media/"
-#endif
+#ifndef __ANDROID__
+	#ifndef _WINRT
+		#define RESOURCE_PATH "../../demos/media/"
+	#else
+		#define RESOURCE_PATH "media/"
+	#endif
 #elif defined(__APPLE__)
-#define RESOURCE_PATH "media/"
+	#define RESOURCE_PATH "media/"
 #else
-#define RESOURCE_PATH "./"
+	#define RESOURCE_PATH "./"
 #endif
 
 #include <stdio.h>
@@ -118,7 +118,7 @@ void __aprilApplicationInit()
 	try
 	{
 		hlog::setLevelDebug(true);
-#if defined(_ANDROID) || defined(_IOS) || defined(_WINRT)
+#if defined(__ANDROID__) || defined(_IOS) || defined(_WINRT)
 		drawRect.setSize(april::getSystemInfo().displayResolution);
 #endif
 		april::init(april::RenderSystemType::Default, april::WindowType::Default);
